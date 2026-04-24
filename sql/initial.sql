868945dd7ba5e0454866b76e503fca1581856353
xtu-sjh <3508285892@qq.com>
2026-04-24 21:21:38 +0800

    fix:修改用户表,文档主表,目录表
    加了一些注释,指定了表的存储引擎,utf8编码

--- sql/initial.sql
+++ sql/initial.sql
@@ -5,28 +5,42 @@ create table user(
     password varchar (100) not null comment '加密密码',
     nickname varchar(50) comment '用户昵称',
     email varchar(100) comment '用户邮箱',
-    create_time datetime default current_timestamp comment '创建时间'
-);
+    avatar varchar(255) default '' comment '头像',
+    status tinyint not null default '1' comment '状态 1正常 0禁用',
+    phone varchar(30) default '' comment '手机号',
+    create_time datetime not null default current_timestamp comment '创建时间',
+    update_time datetime not null default current_timestamp on update current_timestamp COMMENT '更新时间',
+    primary key (id),
+    unique key uk_username(username)
+)engine=innodb default charset=utf8mb4 comment='用户表';
 
---文档表
+--文档主表
 create table document (
-    id bigint auto_increment primary key comment '文档ID',
+    id bigint not null auto_increment comment '文档ID',
     title varchar(200) not null comment '文档标题',
-    user_id bigint not null comment '创建用户ID',
+    creator_id bigint not null comment '创建人ID',
     content longtext comment '文档内容',
-    directory_id bigint not null comment '所属目录ID',
+    folder_id bigint not null comment '所属目录ID',
+    status tinyint not null default '1' comment '1正常 0删除',
     create_time datetime default current_timestamp comment '创建时间',
     update_time datetime default current_timestamp on update current_timestamp comment '更新时间',
-);
+    primary key (id),
+    key idx_folder_id (folder_id),
+    key idx_creator_id (creator_id)
+)engine=innodb default charset=utf8mb4 comment='文档主表';
 
---目录表
+--文档目录表
 create table directory(
-    id ,
-    name varchar(100) not null,
-    parent_id ,
-    user_id not null,
+    id bigint not null auto_increment comment '目录ID',
+    name varchar(100) not null comment '目录名称',
+    sort int not null default '0' comment '排序',
+    creator_id bigint not null comment '创建人id',
+    parent_id bigint not null default '0' comment '父目录id 0=根目录',
     create_time datetime default current_timestamp,
-);
+    update_time datetime not null default current_timestamp on update current_timestamp,
+    primary key (id),
+    key idx_parent_id (parent_id)
+)engine=innodb default charset=utf8mb4 comment='文档目录表';
 
 --文档版本表
 create table document_version (
@@ -38,7 +52,7 @@ create table document_version (
     create_time datetime default current_timestamp,
     version_no int not null ,
     comment varchar(300) ,
-);
+)engine=innodb default charset=utf8mb4 comment='文档版本表';
 
 --权限表
 create table permission (
@@ -49,4 +63,4 @@ create table permission (
     permission_type int not null,
     created_at datetime default current_timestamp ,
     foreign key (uesr_id) references user(id),
-)
+)engine=innodb default charset=utf8mb4 comment='文档权限表';
